eac52c70304fa4e1aa25ed6f5263330a4bec640b
Shakti Ranjan Debata <debatashaktiranjan@gmail.com>
2022-06-16 14:05:20 +0530

    view all er page added

---
+++ view.php
@@ -0,0 +1,67 @@
+<?php
+
+include "utils/conn.php";
+$sql = "SELECT * FROM `er_data`";
+$res = mysqli_query($conn, $sql);
+
+function getUserName($id)
+{
+    $sql = "SELECT name FROM `employee` WHERE eID=$id";
+    $res = mysqli_query($GLOBALS["conn"], $sql);
+    $about = mysqli_fetch_assoc($res);
+    return $about["name"];
+}
+
+?>
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/css/bootstrap.min.css">
+    <title>View All ER</title>
+</head>
+
+<body>
+    <?php include_once "utils/navbar.php" ?>
+    <div class="container mt-3">
+        <table class="table">
+            <thead>
+                <tr>
+                    <th scope="col">#</th>
+                    <th scope="col">Title</th>
+                    <th scope="col">Descriptions</th>
+                    <th scope="col">Category</th>
+                    <th scope="col">Priority</th>
+                    <th scope="col">Effort</th>
+                    <th scope="col">Pod</th>
+                    <th scope="col">Assigned to</th>
+                    <th scope="col">View File</th>
+                </tr>
+            </thead>
+            <tbody>
+                <?php
+                $count = 1;
+                while ($data = mysqli_fetch_assoc($res)) {
+                    echo '<tr>
+                    <th scope="row">' . $count++ . '</th>
+                    <td>' . $data["title"] . '</td>
+                    <td>' . $data["descriptions"] . '</td>
+                    <td>' . $data["category"] . '</td>
+                    <td>' . $data["priority"] . '</td>
+                    <td>' . $data["effort"] . '</td>
+                    <td>' . $data["pod"] . '</td>
+                    <td>' . getUserName($data["assignTo"]) . '</td>
+                    <td>' . $data["file"] . '</td>
+                    </tr>';
+                }
+                ?>
+            </tbody>
+        </table>
+    </div>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/js/bootstrap.bundle.min.js"></script>
+</body>
+
+</html>
